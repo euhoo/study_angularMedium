420607c160e3612fc971a3a9923e862d5b652ab9
Evgeniy Kvach <ypkvach@mts.ru>
2021-03-14 14:49:16 +0300

    add simple store

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { RegisterComponent } from 'src/app/auth/components/register/register.component';
+import { environment } from '../../environments/environment';
 
 const routes: Routes = [{ path: 'register', component: RegisterComponent }];
 
@@ -12,6 +15,12 @@ const routes: Routes = [{ path: 'register', component: RegisterComponent }];
     CommonModule,
     RouterModule.forChild(routes), // регистрация маршрутов в этом модуле
     ReactiveFormsModule,
+    StoreModule.forRoot({}), // регистрация ngrx store и редьюсеров (аргументы функции)
+    /* регистрация redux dev tools */
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // количество action, который хотим видеть
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   declarations: [RegisterComponent],
 })
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../../store/actions/register.action';
 
 @Component({
   selector: 'mc-register', // mc - префикс для того, чтобы понимать, что это наш,а не сторонний компонент
@@ -8,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
   ngOnInit(): void {
     this.initForm();
   }
@@ -20,6 +22,8 @@ export class RegisterComponent implements OnInit {
       password: '',
     });
   }
-
-  onSubmit(): void {}
+  onSubmit(): void {
+    this.store.dispatch(registerAction(this.form.value));
+    console.log(this.store);
+  }
 }
