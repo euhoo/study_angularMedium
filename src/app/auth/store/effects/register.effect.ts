133e36b6782c7fca0c5147d9b555cce44f0bd1a3
Evgeniy Kvach <ypkvach@mts.ru>
2021-03-14 21:04:09 +0300

    better registration fetch

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -5,11 +5,13 @@ import {
   registerFailureAction,
   registerSuccessAction,
 } from '../actions/register.action';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
 import { CurrentUserInterface } from '../../../shared/types/currentUser.interface';
 import { of } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
+import { PersistanceService } from '../../../shared/services/persistance.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class RegisterEffect {
@@ -21,17 +23,34 @@ export class RegisterEffect {
         // запускаем новый стрим
         this.authService.register(request).pipe(
           // подписка на новый стрим, который вернет функция authService.register
-          map(
-            (currentUser: CurrentUserInterface) =>
-              registerSuccessAction({ currentUser }) // если все хорошо - диспатчить success action
-          ),
+          map((currentUser: CurrentUserInterface) => {
+            this.persistanceService.set('accessToken', currentUser.token);
+            return registerSuccessAction({ currentUser }); // если все хорошо - диспатчить success action
+          }),
+          // если упали - диспатчить failure action
           catchError((errResponse: HttpErrorResponse) =>
             of(registerFailureAction({ errors: errResponse.error.errors }))
-          ) // если упали - диспатчить failure action
+          )
         )
       )
     )
   );
-
-  constructor(private actions$: Actions, private authService: AuthService) {}
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/'); // редирект при успешной регистрации
+        })
+      ),
+    /* Каждый createEffect внутри должен диспатчить какой-то action.
+     Если диспатчить не нужно(например как тут), нужно указать что не будет dispatch */
+    { dispatch: false }
+  );
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private persistanceService: PersistanceService,
+    private router: Router
+  ) {}
 }
