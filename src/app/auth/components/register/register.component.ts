d7851d8d7cf0b72c32817608711e98ae1cd817dc
Evgeniy Kvach <ypkvach@mts.ru>
2021-03-14 15:25:21 +0300

    add ngrx selector

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { registerAction } from '../../store/actions/register.action';
+import { Observable } from 'rxjs';
+import { isSubmittingSelector } from '../../store/selectors';
 
 @Component({
   selector: 'mc-register', // mc - префикс для того, чтобы понимать, что это наш,а не сторонний компонент
@@ -10,9 +12,19 @@ import { registerAction } from '../../store/actions/register.action';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
+  isSubmitDisabled: boolean;
+
   constructor(private fb: FormBuilder, private store: Store) {}
   ngOnInit(): void {
     this.initForm();
+    this.initValues();
+  }
+  initValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector)); // можно или так через pipe "async" в html
+    this.isSubmitting$.subscribe((data) => {
+      this.isSubmitDisabled = data; // или вот так напрямую переменную
+    });
   }
 
   initForm(): void {
