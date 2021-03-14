2ca5599caaab769f07813ea3bea50f8194e16336
Evgeniy Kvach <ypkvach@mts.ru>
2021-03-14 15:07:01 +0300

    add first reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}), // регистрация ngrx store и редьюсеров (аргументы функции)
+    /* регистрация redux dev tools */
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // количество action, который хотим видеть
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { RegisterComponent } from 'src/app/auth/components/register/register.component';
-import { environment } from '../../environments/environment';
+import { reducers } from './store/reducers';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [{ path: 'register', component: RegisterComponent }];
 
@@ -15,12 +14,7 @@ const routes: Routes = [{ path: 'register', component: RegisterComponent }];
     CommonModule,
     RouterModule.forChild(routes), // регистрация маршрутов в этом модуле
     ReactiveFormsModule,
-    StoreModule.forRoot({}), // регистрация ngrx store и редьюсеров (аргументы функции)
-    /* регистрация redux dev tools */
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // количество action, который хотим видеть
-      logOnly: environment.production, // Restrict extension to log-only mode
-    }),
+    StoreModule.forFeature('auth', reducers), // подключение редьюсеров с именем поля(auth) объекта
   ],
   declarations: [RegisterComponent],
 })
