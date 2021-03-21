0110e3a38f99d28dd14241cd631a17ecc7efc601
Evgeniy Kvach <ypkvach@mts.ru>
2021-03-21 14:08:26 +0300

    article

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -9,6 +9,9 @@ import { ArticleService as SharedArticleService } from '../shared/services/artic
 import { RouterModule } from '@angular/router';
 import { ErrorMessageModule } from '../shared/modules/errorMessage/errorMessage.module';
 import { LoadingModule } from '../shared/modules/loading/loading.module';
+import { TagListModule } from '../shared/modules/tagList/tagList.module';
+
+const routes = [{ path: 'articles/:slug', component: ArticleComponent }];
 
 @NgModule({
   imports: [
@@ -18,9 +21,11 @@ import { LoadingModule } from '../shared/modules/loading/loading.module';
     RouterModule,
     ErrorMessageModule,
     LoadingModule,
+    RouterModule.forChild(routes),
+    TagListModule,
   ],
   declarations: [ArticleComponent],
-  exports: [ArticleComponent],
+  exports: [],
   providers: [SharedArticleService],
 })
 export class ArticleModule {}
--- src/app/article/compomemts/article/article.component.ts
+++ src/app/article/compomemts/article/article.component.ts
@@ -1,8 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { getArticleAction } from '../../store/actions/getArticle';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, Observable, Subscription } from 'rxjs';
+import { ArticleInterface } from '../../../shared/types/article.interface';
+
+import {
+  articleSelector,
+  errorSelector,
+  isLoadingSelector,
+} from '../../store/selectors';
+import { map } from 'rxjs/operators';
+import { currentUserSelector } from '../../../auth/store/selectors';
 
 @Component({
   selector: 'mc-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent {}
+export class ArticleComponent implements OnInit, OnDestroy {
+  slug: string;
+  article: ArticleInterface | null;
+  sub: Subscription;
+  isLoading$: Observable<boolean>;
+  error$: Observable<string | null>;
+  isAuthor$: Observable<boolean>;
+
+  constructor(private store: Store, private route: ActivatedRoute) {}
+
+  initValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug');
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.error$ = this.store.pipe(select(errorSelector));
+    this.isAuthor$ = combineLatest([
+      this.store.pipe(select(articleSelector)),
+      this.store.pipe(select(currentUserSelector)),
+    ]).pipe(
+      map(([article, currentUser]) => {
+        if (!article || !currentUser) {
+          return false;
+        }
+        return article?.author.username === currentUser?.username;
+      })
+    );
+  }
+  initListeners(): void {
+    this.sub = this.store.pipe(select(articleSelector)).subscribe((article) => {
+      this.article = article;
+    });
+  }
+  fetchData(): void {
+    this.store.dispatch(getArticleAction({ slug: this.slug }));
+  }
+  ngOnInit(): void {
+    this.initValues();
+    this.initListeners();
+    this.fetchData();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+}
